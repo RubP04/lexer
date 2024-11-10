bedf9840aa2d68fed4512cc5ddbf284b5996ca96
RubP04 <rubenruben1521@gmail.com>
2024-11-09 20:35:43 -0500

    turned token patterns into a list of tuples instead

--- lexer.py
+++ lexer.py
@@ -2,16 +2,14 @@ import re
 from enum import Enum
 from typing import List, Tuple, Dict
 
-TOKEN_PATTERNS = {
-    'KEYWORD' : r'\b(int|bool|float|char|if|else|while|true|false|main)\b',
-    'IDENTIFIER' : r'[a-zA-Z_][a-zA-Z0-9_]*',
-    'INTEGER' : r'\b\d+\b',
-    'FLOAT' : r'\b\d+\.\d+\b',
-    'CHAR' : r"'(?:\\.|[^'\\])'",
-    'OPERATOR' : r'==|!=|<=|>=|\+|-|\*|/|%|=|<|>|!',
-    'DELIMITER' : r'[{}();,]',
-    'LOGICAL_OP' : r'&&|\|\|',
-    'BRACKET' : r'[\[\]]'
-}
-
-TOKEN_ORDER = ['KEYWORD', 'LOGICAL_OP', 'OPERATOR', 'FLOAT', 'INTEGER', 'CHAR', 'BRACKET', 'DELIMITER']
+TOKEN_PATTERS = [
+    ('KEYWORD', r'\b(int|bool|float|char|if|else|while|true|false|main)\b'),
+    ('LOGICAL_OP', r'&&|\|\|'),
+    ('OPERATOR', r'==|!=|<=|>=|\+|-|\*|/|%|=|<|>|!'),
+    ('FLOAT', r'\b\d+\.\d+\b'),
+    ('INTEGER', r'\b\d+\b'),
+    ('CHAR', r"'(?:\\.|[^'\\])'"),
+    ('BRACKET', r'[\[\]]'),
+    ('DELIMITER', r'[{}();,]'),
+    ('IDENTIFIER', r'[a-zA-Z_][a-zA-Z0-9_]*')
+]
